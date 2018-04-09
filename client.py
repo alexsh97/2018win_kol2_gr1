bc3df6326cb3327b622513d819c294935de131eb
alexsh97 <alexsh997@gmail.com>
2018-04-09 22:53:54 +0200

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-class client:
-
-	def __init__(self,client_name,client_ID, client_money):
-		self.name = client_name
-		self.ID = client_ID
-		self.money = client_money
-
-	def showInfo(self):
-		print("Name: "+self.name)
